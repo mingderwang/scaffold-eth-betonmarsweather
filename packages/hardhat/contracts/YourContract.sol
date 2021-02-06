6a7b4141569463d624296890e11567cfef3ee5ae
Ming-der Wang <mingderwang@gmail.com>
2021-02-06 22:10:35 +0800

    tmp, FIXed Token in YourContract

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -3,21 +3,10 @@ pragma solidity >=0.6.0 <0.7.0;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol"; //https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
-
-contract YourContract is Ownable {
-
-  event SetPurpose(address sender, string purpose);
-
-  string public purpose = "🛠 Programming Unstoppable Money";
-
-  constructor() public {
-    // what should we do on deploy?
-  }
-
-  function setPurpose(string memory newPurpose) public onlyOwner {
-    purpose = newPurpose;
-    console.log(msg.sender,"set purpose to",purpose);
-    emit SetPurpose(msg.sender, purpose);
-  }
-
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+ 
+contract YourContract is ERC20, Ownable {
+    constructor() public ERC20("Fixed", "FIX") {
+        _mint(msg.sender, 1000);
+    }
 }
