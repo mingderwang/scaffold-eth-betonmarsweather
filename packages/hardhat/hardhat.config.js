c0cf759ed84e23de7937d9371007cc0f67cdd48a
Ming-der Wang <mingderwang@gmail.com>
2021-02-16 11:48:23 +0800

    use Permittable Fixed token

--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -95,7 +95,7 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.0",
+        version: "0.7.0",
         settings: {
           optimizer: {
             enabled: true,
